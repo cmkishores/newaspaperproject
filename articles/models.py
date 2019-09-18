9eaa480f2e1941eea09b38e420afb42367dc776d
cmkishores <cmkishores@gmail.com>
2019-09-18 12:07:50 +0530

    Created custom user model and model for article app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Article(models.Model):
+	news_title = models.CharField(max_length=50)
+	content = models.textField()
+	author = models.textField(max_length=20)
+
+	def __str__(self):
+		return self.title[:50]
+
+	def get_absolute_url(self):
+		return reverse('article', args=[str(self.id)])
+
 # Create your models here.
