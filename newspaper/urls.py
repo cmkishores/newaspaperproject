837d3f8913d92762ff55cb45258c343b4eac6d17
cmkishores <cmkishores@gmail.com>
2019-09-18 16:16:27 +0530

    Add custom user form, custom user model and created sign up page

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('user/',include('django.contrib.auth.urls')),
+    path('user/',include('users.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 
-     ]
+    ]
