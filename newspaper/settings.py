2e6caaed5ba7b4813812f45213cf4a6ae919b1f9
cmkishores <cmkishores@gmail.com>
2019-09-18 11:48:35 +0530

    Added 2 apps articles and users and configured the settings file. Added templates path to settings file

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*5e$2l-n8929(epngm62#*@0arbaald19vhcmoytu=2p#93z0h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles.apps.ArticlesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'newspaper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
